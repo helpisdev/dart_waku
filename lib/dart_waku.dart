b74976adda83133cc326624dbf6e366deff9508b
Kapareliotis Elias (Καπαρελιώτης Ηλίας) <helpis@tutamail.com>
2023-07-25 19:43:14 +0300

    feat: Expose public API.

---
+++ lib/dart_waku.dart
@@ -0,0 +1,13 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library waku;
+
+export 'src/dart_waku_base.dart'
+    show
+        ReturnCode,
+        WakuCallBack,
+        WakuException,
+        WakuMessage,
+        WakuNode,
+        WakuRelay;
